2d642c50368c6a578588e83c48d19c2b09cd0e14
ADMIN <ADMIN@LAB1-MAY25>
2024-06-10 16:48:53 +0700

    Add project files.

---
+++ Csharp_12 con giap/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Csharp_12_con_giap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnam = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btntinh = new System.Windows.Forms.Button();
+            this.lbltinh = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblnam = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtnam
+            // 
+            this.txtnam.Location = new System.Drawing.Point(203, 97);
+            this.txtnam.Name = "txtnam";
+            this.txtnam.Size = new System.Drawing.Size(100, 20);
+            this.txtnam.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(331, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btntinh
+            // 
+            this.btntinh.Location = new System.Drawing.Point(203, 231);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(75, 23);
+            this.btntinh.TabIndex = 2;
+            this.btntinh.Text = "Tính";
+            this.btntinh.UseVisualStyleBackColor = true;
+            this.btntinh.Click += new System.EventHandler(this.btntinh_Click);
+            // 
+            // lbltinh
+            // 
+            this.lbltinh.AutoSize = true;
+            this.lbltinh.Location = new System.Drawing.Point(94, 104);
+            this.lbltinh.Name = "lbltinh";
+            this.lbltinh.Size = new System.Drawing.Size(33, 13);
+            this.lbltinh.TabIndex = 3;
+            this.lbltinh.Text = "TÍNH";
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(203, 154);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(100, 20);
+            this.txtKQ.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(375, 154);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(297, 173);
+            this.listBox1.TabIndex = 5;
+            // 
+            // lblnam
+            // 
+            this.lblnam.AutoSize = true;
+            this.lblnam.Location = new System.Drawing.Point(94, 161);
+            this.lblnam.Name = "lblnam";
+            this.lblnam.Size = new System.Drawing.Size(29, 13);
+            this.lblnam.TabIndex = 6;
+            this.lblnam.Text = "Năm";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(203, 53);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lblnam);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.lbltinh);
+            this.Controls.Add(this.btntinh);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtnam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btntinh;
+        private System.Windows.Forms.Label lbltinh;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblnam;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Csharp_12 con giap/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_12_con_giap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btntinh_Click(object sender, EventArgs e)
+        {
+            string can = "";
+
+            switch (Convert.ToInt32(txtnam.Text) % 10)
+            {
+                case 0:
+                    can = "canh";
+                    break;
+                case 1:
+                    can = "Tân";
+                    break;
+                case 2:
+                    can = "Nhâm";
+                    break;
+                case 3:
+                    can = "Quý";
+                    break;
+                case 4:
+                    can = "Giáp";
+                    break;
+                case 5:
+                    can = "Ất";
+                    break;
+                case 6:
+                    can = "Bính";
+                    break;
+                case 7:
+                    can = "Đinh";
+                    break;
+                case 8:
+                    can = "Mậu";
+                    break;
+                case 9:
+                    can = "Kỷ";
+                    break;
+            }
+            string chi = "";
+            switch (Convert.ToInt32(txtnam.Text) % 12) 
+            {
+                case 0:
+                    chi = "Thân";
+                    break;
+                case 1:
+                    chi = "Đậu";
+                    break;
+                case 2:
+                    chi = "Tuất";
+                    break;
+                case 3:
+                    chi = "Hợi";
+                    break;
+                case 4:
+                    chi = "Tý";
+                    break;
+                case 5:
+                    chi = "SỬU";
+                    break;
+                case 6:
+                    chi = "DẦN";
+                    break;
+                case 7:
+                    chi = "Mão";
+                    break;
+                case 8:
+                    chi = "Thình";
+                    break;
+                case 9:
+                    chi = "Tỵ";
+                    break;
+                case 10:
+                    chi = "Ngọ";
+                    break;
+                case 11:
+                    chi = "Mùi";
+                    break;
+            }
+            txtKQ.Text = can + " " + chi;
+        }
+    }
+}
